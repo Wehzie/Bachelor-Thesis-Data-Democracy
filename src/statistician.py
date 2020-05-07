29138cd3f8b57d9497be05ea4080f717a4384a7b
Rafael <rafael@linux.fritz.box>
2020-05-08 00:54:34 +0200

    notes on future plots

--- src/statistician.py
+++ src/statistician.py
@@ -33,6 +33,12 @@ class Statistician(object):
             },
         }
 
+    # TODO: Create 4 plots showing this
+    # Money: f_money, f_reserve, hh_money
+    # Wage: f_wage, f_marginal_cost, hh_res_wage
+    # Items: f_num_items, f_item_price, f_demand
+    # Employment: f_num_employees, f_months_hiring, hh_employment, hh_num_vendors
+
     ######## ######## ######## METHODS ######## ######## ########
 
     def calc_avg(self):
@@ -57,18 +63,20 @@ class Statistician(object):
         self.hh_stat['avg']['num_vendors'].append(sum([len(hh.vendor_list) for hh in hh_list]) / num_hh)
         self.hh_stat['avg']['res_wage'].append(sum([hh.res_wage for hh in hh_list]) / num_hh)
     
+    # each month notify the statistician of what is going on in the simulation
     def up_stat(self):
         self.calc_avg()
 
     # TODO: think about days within a month
     # plot hh and firm money against time
-    def plot_money_monthly(self):
+    def plot_money_reserve(self):
         x_months = [m for m in range(self.sim.num_months)]
         y1_f_money = self.f_stat['avg']['money']
         y2_hh_money = self.hh_stat['avg']['money']
         y3_f_reserve = self.f_stat['avg']['reserve']
 
         fig, ax = plt.subplots()
+        # BUG: Firm money and firm reserve curves are identical
         ax.plot(x_months, y1_f_money, 'r', label='Firms')
         ax.plot(x_months, y2_hh_money, 'b', label='Households')
         ax.plot(x_months, y3_f_reserve, 'g', label='Firm Reserve')
@@ -87,21 +95,27 @@ class Statistician(object):
         x_months = [m for m in range(self.sim.num_months)]
         y1_num_employees = self.f_stat['avg']['num_employees']
         y2_employment = self.hh_stat['avg']['employment']
+        y3_price = self.f_stat['avg']['item_price']
+        y4_wage = self.f_stat['avg']['wage']
+        y5_months_hiring = self.f_stat['avg']['months_hiring']
 
         fig, ax = plt.subplots()
         ax.plot(x_months, y1_num_employees, 'r', label='Average number of firm employees')
-        ax.plot(x_months, y2_employment, 'r', label='Household employment rate')
+        ax.plot(x_months, y2_employment, 'g', label='Household employment rate')            # BUG: 100% (1.00)
+        ax.plot(x_months, y3_price, 'b', label='Item price')
+        ax.plot(x_months, y4_wage, 'k', label='Wage')
+        ax.plot(x_months, y5_months_hiring, 'm', label='Months Hiring')
 
-        ax.set(xlabel='Months', ylabel='Employment',
-            title='Household Employment')
+        ax.set(xlabel='Months', ylabel='Price, wage, employment',
+            title='Price, wage, employment')
         ax.grid()
         ax.legend()
 
-        fig.savefig('employment.png')
+        fig.savefig('price_wage_employment.png')
         plt.show()
 
     def invoke_plots(self):
-        self.plot_money_monthly()
+        self.plot_money_reserve()
         self.plot_price_wage_employment()
 
 ######## ######## ######## IMPORTS ######## ######## ########
